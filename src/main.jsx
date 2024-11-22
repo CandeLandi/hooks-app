d73e195c4f524527465bfde857a80dd79fcf105f
Candela Landi <landicandela01@gmail.com>
2024-11-22 10:58:34 -0300

    add todoReducer

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom/client";
 import { HooksApp } from "./HooksApp";
 import { CounterWithCustomHook } from "./01-useState/CounterWithCustomHook";
 import { MultipleCustomHooks } from "./03-examples";
+import { Padre } from "./07-tarea-memo/Padre";
+import { TodoApp } from "./08-useReducer/TodoApp";
+
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
-  <Padre />
-  // </React.StrictMode>
+<React.StrictMode>
+  <TodoApp />
+ </React.StrictMode>
 );
