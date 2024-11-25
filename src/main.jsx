c40a8e5c9153ee7a5138b960d05d94a39c85e951
Candela Landi <landicandela01@gmail.com>
2024-11-25 09:22:52 -0300

    add useContext

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,14 @@ import { MultipleCustomHooks } from "./03-examples";
 import { Padre } from "./07-tarea-memo/Padre";
 import { TodoApp } from "./08-useReducer/TodoApp";
 
+import { BrowserRouter } from "react-router-dom";  
+import { MainApp } from "./09-useContext/MainApp";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-<React.StrictMode>
-  <TodoApp />
- </React.StrictMode>
+  <BrowserRouter>
+    {/* <React.StrictMode> */}
+      <MainApp />
+    {/* </React.StrictMode> */}
+  </BrowserRouter>
 );
